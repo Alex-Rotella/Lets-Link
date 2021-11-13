e8f99ad7560f571f1aa561265d281b54b7e4f94f
BatuhanKir <47462383+BatuhanKir@users.noreply.github.com>
2021-11-13 02:16:07 -0500

    mobile-bugs

--- src/components/HomeSection/index.js
+++ src/components/HomeSection/index.js
@@ -1,32 +1,41 @@
-import React from 'react';
-import '../../App.css';
-import './HomeSection.css';
-import { Button } from '../Button';
+import React from "react";
+import "../../App.css";
+import "./HomeSection.css";
+import { Button } from "../Button";
 import { useHistory } from "react-router-dom";
 
-
 function HomeSection() {
-  
   const history = useHistory();
   function getStarted() {
-        history.push({
-          pathname: "/Form"
-          }
-        )
-      }
-  
+    history.push({
+      pathname: "/Form",
+    });
+  }
+
   return (
-    <div className='hero-container'>
-      <div className="fullscreen-video-wrap" dangerouslySetInnerHTML={{ __html: `
-      <video src='/videos/bonfire.mp4' autoPlay loop muted />
-      ` }}></div>
+    <div className="hero-container">
+      <div
+        className="fullscreen-video-wrap"
+        dangerouslySetInnerHTML={{
+          __html: `
+      <video 
+        loop
+        muted
+        autoPlay
+        playsinline
+      >
+      <source src="/videos/bonfire.mp4" type="video/mp4">
+      </video>
+      `,
+        }}
+      ></div>
       <h1> Let's Link </h1>
       <p>What are you waiting for?</p>
-      <div className='hero-btns'>
-      <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
           onClick={getStarted}
         >
           GET STARTED
