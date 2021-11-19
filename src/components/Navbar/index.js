673a105dc2d23b83c50b4a6c840103782c7220b3
Alex-Rotella <arotella1221@gmail.com>
2021-11-18 20:07:28 -0500

    About page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -40,7 +40,7 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+              <Link to="/About" className="nav-links" onClick={closeMobileMenu}>
                 About
               </Link>
             </li>
