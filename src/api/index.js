67c330aa31703b7e235e448dafa4641fc56d1b6c
BatuhanKir <47462383+BatuhanKir@users.noreply.github.com>
2021-12-04 16:58:50 -0500

    pulling-images

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-//261994b778msh032e8e575442ca5p10f2e3jsn8a007c096b14
-//e65de07aecmsh675c628cd2757f3p1a3e1ejsnc5163409a5e4
 export const getRestaurantsData = async (lat, lng) => {
   try {
     const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng`, {
@@ -9,7 +7,7 @@ export const getRestaurantsData = async (lat, lng) => {
         longitude: lng,
       },
       headers: {
-        'x-rapidapi-key': '261994b778msh032e8e575442ca5p10f2e3jsn8a007c096b14',
+        'x-rapidapi-key': 'e65de07aecmsh675c628cd2757f3p1a3e1ejsnc5163409a5e4',
         'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
       },
     });
@@ -28,7 +26,7 @@ export const getAttractionsData = async (lat, lng) => {
           longitude: lng,
         },
         headers: {
-          'x-rapidapi-key': '261994b778msh032e8e575442ca5p10f2e3jsn8a007c096b14',
+          'x-rapidapi-key': 'e65de07aecmsh675c628cd2757f3p1a3e1ejsnc5163409a5e4',
           'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
         },
       });
