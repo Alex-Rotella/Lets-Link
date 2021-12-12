a18db87b1b2d8791c1dff1289e37395a837f470a
Alex-Rotella <arotella1221@gmail.com>
2021-12-12 18:22:49 -0500

    About layout

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -18,7 +18,45 @@ function Cards() {
             />
 
           </ul>
-          
+          <ul className='cards__items'>
+            <CardItem
+              src='images/nightOut.jpeg'
+              text='LETSLINK was developed to plan your next night in town. If you ever had trouble deciding what to do on your night off with friends
+                     and family, LETLINK will solve your problem. No more wasting time with undecisveness, LETSLINK today! 
+                   '
+              label='LETSLINK'
+            />
+              <CardItem
+              src='images/nightOut.jpeg'
+              text='LETSLINK was developed to plan your next night in town. If you ever had trouble deciding what to do on your night off with friends
+                     and family, LETLINK will solve your problem. No more wasting time with undecisveness, LETSLINK today! 
+                   '
+              label='LETSLINK'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/nightOut.jpeg'
+              text='LETSLINK was developed to plan your next night in town. If you ever had trouble deciding what to do on your night off with friends
+                     and family, LETLINK will solve your problem. No more wasting time with undecisveness, LETSLINK today! 
+                   '
+              label='LETSLINK'
+            />
+              <CardItem
+              src='images/nightOut.jpeg'
+              text='LETSLINK was developed to plan your next night in town. If you ever had trouble deciding what to do on your night off with friends
+                     and family, LETLINK will solve your problem. No more wasting time with undecisveness, LETSLINK today! 
+                   '
+              label='LETSLINK'
+            />
+                          <CardItem
+              src='images/nightOut.jpeg'
+              text='LETSLINK was developed to plan your next night in town. If you ever had trouble deciding what to do on your night off with friends
+                     and family, LETLINK will solve your problem. No more wasting time with undecisveness, LETSLINK today! 
+                   '
+              label='LETSLINK'
+            />
+                      </ul>
         </div>
       </div>
     </div>
